82bae456f59f157679f2e4b9afb352520c78b199
Alex Christy <a.christy@ufl.edu>
2025-01-15 21:29:17 -0500

    Add username generation custom module

---
+++ library/tests/test_username_generation.py
@@ -0,0 +1,216 @@
+import logging
+import re
+
+import pytest
+
+from library.username_generation import (
+    gen_scheme_username,
+    gen_seeded_username,
+    validate_username_scheme,
+)
+
+###### validate_username_scheme tests ######
+
+
+def test_validate_username_scheme_valid() -> None:
+    """Test validate_username_scheme with valid scheme."""
+    valid_schemes = [
+        "\\f.\\l+",
+        "\\f\\l+",
+        "\\f\\l+\\d",
+        "\\f\\f.\\l+",
+        "\\f\\f\\l+",
+        "\\f\\f\\l+\\d\\d\\d",
+        "",
+        "\\f",
+    ]
+
+    for scheme in valid_schemes:
+        assert validate_username_scheme(scheme)
+
+
+def test_validate_username_scheme_invalid() -> None:
+    """Test validate_username_scheme with invalid scheme."""
+    invalid_schemes = {
+        "\\": "Missing meta character after backslash.",
+        "\\o": "Invalid meta character 'o'.",
+        "\\d+": "Invalid quantifier '+' placement.",
+        "+": "Quantifier '+' placed at the beginning of the scheme.",
+    }
+
+    for scheme, reason in invalid_schemes.items():
+        assert validate_username_scheme(scheme) is False, reason
+
+
+###### gen_scheme_username tests ######
+
+
+def test_valid_scheme_username_no_digits() -> None:
+    """Test gen_scheme_username with valid scheme with no digits."""
+    first_name = "John"
+    last_name = "Smith"
+
+    tests = {
+        "\\f.\\l+": "j.smith",
+        "\\f\\l+": "jsmith",
+        "\\f+\\l": "johns",
+        "\\f\\f.\\l+": "jo.smith",
+        "\\f\\f\\l+": "josmith",
+        "\\f+\\f\\l": "johns",  # Once the firstname is consumed no more characters are available
+        "\\f.\\l+\\l": "j.smith",  # Once the lastname is consumed no more characters are available
+        "\\f-\\l+": "j-smith",
+        "\\l.\\f+": "s.john",
+        "\\l\\f+": "sjohn",
+        "\\l+\\f": "smithj",
+        "\\f\\f\\f\\f\\f": "john",
+    }
+
+    for scheme, expected in tests.items():
+        assert (
+            gen_scheme_username(first_name, last_name, scheme, lower=True) == expected
+        )
+
+
+def test_valid_digit_username_schemes() -> None:
+    """Test gen_scheme_username with valid digit schemes."""
+    first_name = "John"
+    last_name = "Smith"
+
+    tests = {
+        "\\f\\d": r"j\d",
+        "\\f\\d\\d": r"j\d\d",
+        "\\f\\d\\d\\d": r"j\d\d\d",
+        "\\d\\f": r"\dj",
+        "\\f.\\l+\\d": r"j.smith\d",
+        "\\f.\\l+\\l\\d": r"j.smith\d",
+    }
+
+    for scheme, regex in tests.items():
+        pattern = re.compile(regex)
+        assert pattern.match(
+            gen_scheme_username(first_name, last_name, scheme, lower=True)
+        )
+
+
+def test_invalid_username_schemes() -> None:
+    """Test gen_scheme_username with invalid schemes."""
+    first_name = "John"
+    last_name = "Smith"
+
+    invalid_schemes = {
+        "\\": "Missing meta character after backslash.",
+        "\\o": "Invalid meta character 'o'.",
+        "\\d+": "Invalid quantifier '+' placement.",
+        "+": "Quantifier '+' placed at the beginning of the scheme.",
+    }
+
+    for scheme, reason in invalid_schemes.items():
+        try:
+            with pytest.raises(ValueError):
+                gen_scheme_username(first_name, last_name, scheme, lower=True)
+        except AssertionError:
+            pytest.fail(
+                f"Expected ValueError was not raised for invalid scheme: {scheme} Reason: {reason}"
+            )
+
+
+def test_missing_first_name() -> None:
+    """Test ValueError is raised when first_name arg is not empty."""
+    with pytest.raises(ValueError, match="First name and last name must not be empty."):
+        gen_scheme_username("", "Smith", "\\f.\\l+")
+
+
+def test_missing_last_name() -> None:
+    """Test ValueError is raised when last_name arg is not empty."""
+    with pytest.raises(ValueError, match="First name and last name must not be empty."):
+        gen_scheme_username("John", "", "\\f.\\l+")
+
+
+def test_empty_scheme_warning(caplog: pytest.LogCaptureFixture) -> None:
+    """Test that a warning is provided to users when an empty scheme is provided."""
+    with caplog.at_level(logging.WARNING):
+        gen_scheme_username("John", "Smith", "")
+
+    assert "Scheme is empty. Output will always be empty." in caplog.text
+    assert any(record.levelname == "WARNING" for record in caplog.records)
+
+
+def test_gen_seeded_username_empty_seed() -> None:
+    """Test that seeded usernames are generated correctly when only one name is seeded."""
+    empty_seeds = ["", " ", "   "]
+
+    tests = {
+        "\\f.\\l+": r"[a-z].[a-z]+",
+        "\\f+.\\l": r"[a-z]+.[a-z]",
+        "\\f.\\l\\l\\l\\d\\d": r"[a-z].[a-z]{3}\d\d",
+        "\\f\\l+\\d\\d\\d": r"[a-z]+\d\d",
+    }
+
+    for seed in empty_seeds:
+        for scheme, regex in tests.items():
+            pattern = re.compile(regex)
+            assert pattern.match(gen_seeded_username(seed, scheme, lower=True))
+
+
+def test_gen_seeded_username_first_only() -> None:
+    """Test that seeded usernames are generated correctly when only one name is seeded."""
+    name = "John"
+
+    tests = {
+        "\\f.\\l+": r"j.[a-z]+",
+        "\\f+.\\l": r"john.[a-z]",
+        "\\f.\\l\\l\\l\\d\\d": r"j.[a-z]{3}\d\d",
+        "\\f\\l+\\d\\d\\d": r"j[a-z]+\d\d",
+    }
+
+    for scheme, regex in tests.items():
+        pattern = re.compile(regex)
+        assert pattern.match(gen_seeded_username(name, scheme, lower=True))
+
+
+def test_gen_seeded_username_first_and_last() -> None:
+    """Test that seeded usernames are generated correctly when first and last names are seeded."""
+    name = "John Smith"
+
+    tests = {
+        "\\f.\\l+": r"j.smith",
+        "\\f+.\\l": r"john.s",
+        "\\f.\\l\\l\\l\\d\\d": r"j.smi\d\d",
+        "\\f\\l+\\d\\d\\d": r"jsmith\d\d",
+    }
+
+    for scheme, regex in tests.items():
+        pattern = re.compile(regex)
+        assert pattern.match(gen_seeded_username(name, scheme, lower=True))
+
+
+def test_gen_seeded_username_first_middle_and_last() -> None:
+    """Test that seeded usernames are generated correctly when first, middle, and last names are seeded."""
+    name = "John Adam Smith"
+
+    tests = {
+        "\\f.\\l+": r"j.smith",
+        "\\f+.\\l": r"john.s",
+        "\\f.\\l\\l\\l\\d\\d": r"j.smi\d\d",
+        "\\f\\l+\\d\\d\\d": r"jsmith\d\d",
+    }
+
+    for scheme, regex in tests.items():
+        pattern = re.compile(regex)
+        assert pattern.match(gen_seeded_username(name, scheme, lower=True))
+
+
+def test_gen_seeded_username_more_than_first_middle_and_last() -> None:
+    """Test that seeded usernames are generated correctly when more than first, middle, and last names are seeded."""
+    name = "John Quincy Adam Smith"
+
+    tests = {
+        "\\f.\\l+": r"j.smith",
+        "\\f+.\\l": r"john.s",
+        "\\f.\\l\\l\\l\\d\\d": r"j.smi\d\d",
+        "\\f\\l+\\d\\d\\d": r"jsmith\d\d",
+    }
+
+    for scheme, regex in tests.items():
+        pattern = re.compile(regex)
+        assert pattern.match(gen_seeded_username(name, scheme, lower=True))
---
+++ library/username_generation.py
@@ -0,0 +1,289 @@
+import logging
+from random import randint
+
+import names as name_db
+from ansible.module_utils.basic import AnsibleModule
+
+logger = logging.getLogger(__name__)
+
+# Constants
+FIRST_NAME_META_CHAR = "f"
+LAST_NAME_META_CHAR = "l"
+DIGIT_META_CHAR = "d"
+FULL_QUANTIFIER = "+"
+
+# Grouped constants
+META_CHARS = [FIRST_NAME_META_CHAR, LAST_NAME_META_CHAR, DIGIT_META_CHAR]
+QUANTFIABLE_META_CHARS = [FIRST_NAME_META_CHAR, LAST_NAME_META_CHAR]
+QUANTIFIERS = [FULL_QUANTIFIER]
+
+
+def validate_username_scheme(scheme: str) -> bool:
+    r"""Validate the username scheme.
+
+    The scheme is defined with a rudimentary regex pattern. Accepts arbitrary
+    characeters and the following placeholders:
+
+    Scheme Regex Character Classes:
+    ------------------------------
+    \f: Letter of a firstname.
+    \l: Letter of a lastname.
+    \d: Random digit 0 to 9.
+
+    Quantifiers:
+    -----------
+    +: Entire name. Used with \f or \l.
+
+    Args:
+    ----
+        scheme (str): Username scheme.
+
+    Returns:
+    -------
+        bool: True if the scheme is valid, False otherwise.
+
+    """
+    # Validate meta characters
+    meta_char_parts = scheme.split("\\")
+
+    # First split can be ignored
+    for part in meta_char_parts[1:]:
+        try:
+            if part[0] not in META_CHARS:
+                logger.error("Invalid meta character: '%s' ", part[0])
+                return False
+        except Exception as e:
+            logger.error("An unexpected error occurred: %s", e)
+            return False
+
+    # Validate quantifiers
+    for quantifier in QUANTIFIERS:
+        quantifier_parts = scheme.split(quantifier)
+
+        # Last split can be ignored
+        for part in quantifier_parts[:-1]:
+            try:
+                if part[-1] not in QUANTFIABLE_META_CHARS:
+                    logger.error(
+                        "Invalid quantifier placement. Quantifier: '%s' placed after non-quantifiable character: '%s' ",
+                        quantifier,
+                        part[-1],
+                    )
+                    return False
+            except IndexError:
+                logger.error(
+                    "Invalid quantifier placement. Quantifier: '%s' placed at the beginning of the scheme.",
+                    quantifier,
+                )
+                return False
+            except Exception as e:
+                logger.error("An unexpected error occurred: %s", e)
+                return False
+
+    logger.info("Username scheme: '%s' is valid.", scheme)
+
+    return True
+
+
+def gen_scheme_username(
+    first_name: str,
+    last_name: str,
+    scheme: str,
+    lower: bool = True,
+) -> str:
+    r"""Generate a username according to the scheme.
+
+    The scheme is defined with a rudimentary regex pattern. Accepts arbitrary
+    characeters and the following placeholders:
+
+    Scheme Regex Character Classes:
+    ------------------------------
+    \\f: Letter of a firstname.
+    \\l: Letter of a lastname.
+    \\d: Random digit 0 to 9.
+
+    Quantifiers:
+    -----------
+    +: Entire name. Used with \\f or \\l. (Puts the rest of the name if \\f has been used earlier in the scheme.)
+
+    Args:
+    ----
+        first_name (str): First name of the user.
+        last_name (str): Last name of the user.
+        scheme (str): Username scheme.
+        lower (bool): Convert the generated username to lowercase. Default is True.
+
+    Returns:
+    -------
+        str: Generated username.
+
+    Raises:
+    ------
+        ValueError: If the scheme is invalid or if first_name or last_name are empty.
+
+    """
+    # Validate the scheme
+    if not validate_username_scheme(scheme):
+        msg = f"Invalid username scheme: '{scheme}'."
+        logger.error(msg)
+        raise ValueError(msg)
+
+    # Validate the first and last name
+    if not first_name or not last_name:
+        msg = "First name and last name must not be empty."
+        logger.error(msg)
+        raise ValueError(msg)
+
+    if not scheme:
+        logger.warning("Scheme is empty. Output will always be empty.")
+
+    # Generate the username
+    scheme_parts = scheme.split("\\")
+
+    # First part can't be a meta character
+    username = scheme_parts[0]
+
+    for part in scheme_parts[1:]:
+        meta_char = part[0]
+
+        try:
+            has_quantifier = part[1] in QUANTIFIERS
+        except IndexError:
+            has_quantifier = False
+
+        if meta_char == FIRST_NAME_META_CHAR:
+            if has_quantifier:
+                username += first_name
+                first_name = ""  # Remove the first name
+            elif first_name:  # If the username is not empty
+                username += first_name[0]
+                first_name = first_name[1:]  # Remove the first character
+        elif meta_char == LAST_NAME_META_CHAR:
+            if has_quantifier:
+                username += last_name
+                last_name = ""
+            elif last_name:
+                username += last_name[0]
+                last_name = last_name[1:]
+        elif meta_char == DIGIT_META_CHAR:
+            username += str(randint(0, 9))  # noqa: S311 (Not crypto)
+
+        # Remove quantifier from the scheme part
+        if has_quantifier:
+            username += part[2:]
+        else:
+            username += part[1:]
+
+    if lower:
+        username = username.lower()
+
+    return username
+
+
+def gen_seeded_username(name_seed: str, scheme: str, lower: bool = True) -> str:
+    """Generate a username based on a seed and a scheme.
+
+    This function assumes that names follow the structure of First, Middle (Optional), Last. If
+    a name has more than a First, Middle, and Last name then the First name will be selected from
+    the first name provided and the last name will be select from the last name provided.
+
+    Ex:
+    - name="John" --> first_name="John" last_name="{Random last name from census DB}
+    - name="John Smith" --> first_name="John" last_name="Smith"
+    - name="John Adams Smith" --> first_name="John" last_name="Smith"
+    - name="John Quincy Adam Smith" --> first_name="John" last_name="Smith"
+
+    Args:
+    ----
+        name_seed (str): Name that will act as the seed for the generated username.
+        scheme (str): Scheme to generate the username with.
+        lower (bool): Whether or not username should be made lowercase.
+
+    Returns:
+    -------
+        str: Generated username based on seed conforming to scheme.
+
+    Raises:
+    ------
+        ValueError: Scheme is invalid.
+
+    """
+    len_first_only = 1
+    len_first_last = 2
+    len_first_middle_last = 3
+
+    names = name_seed.strip().split(" ")
+
+    # Parse seed
+    if len(names) == len_first_only:
+        if not names[0]:
+            first_name, last_name = name_db.get_full_name().split(" ")
+        else:
+            first_name = names[0]
+            last_name = name_db.get_last_name()
+    elif len(names) == len_first_last:
+        first_name = names[0]
+        last_name = names[1]
+    elif len(names) == len_first_middle_last:
+        first_name = names[0]
+        last_name = names[2]
+    else:
+        first_name = names[0]
+        last_name = names[-1]
+
+    return gen_scheme_username(first_name, last_name, scheme, lower=lower)
+
+
+def run() -> None:  # pragma: no cover
+    """Run the custom Anisble module."""
+    module = AnsibleModule(
+        argument_spec=dict(
+            number=dict(type="int", required=True),
+            scheme=dict(type="str", required=True),
+            name_seeds=dict(type="list", elements="str", required=False),
+            existing_usernames=dict(type="list", elements="str", required=False),
+            lowercase=dict(type="bool", required=False, default=True),
+        )
+    )
+
+    # Ansible args typed
+    name_seeds: list[str] = module.params["name_seeds"]
+    req_num_usernames: int = module.params["number"]
+    scheme: str = module.params["scheme"]
+    existing_usernames = set(module.params["existing_usernames"])
+    lowercase_choice: bool = module.params["lowercase"]
+
+    usernames = existing_usernames.copy()
+
+    try:
+        # Generate seeded usernames
+        for name in name_seeds:
+            if len(usernames - existing_usernames) >= req_num_usernames:
+                break
+
+            usernames.add(gen_seeded_username(name, scheme, lower=lowercase_choice))
+
+        # Generate random usernames to reach requested number of usernames
+        while len(usernames - existing_usernames) < req_num_usernames:
+            first_name, last_name = name_db.get_full_name().split(" ")
+            usernames.add(
+                gen_scheme_username(
+                    first_name, last_name, scheme, lower=lowercase_choice
+                )
+            )
+    except Exception as e:
+        module.fail_json(msg=f"Failed to generate usernames: {e}")
+
+    dedup_usernames = usernames - existing_usernames
+
+    result = {
+        "changed": False,
+        "msg": f"Generated {len(dedup_usernames)} usernames",
+        "usernames": dedup_usernames,
+    }
+
+    module.exit_json(**result)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    run()
